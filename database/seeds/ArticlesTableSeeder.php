580a6174bb8081a0c18961cce3241d200c31e269
a831123c <a831123d@gmail.com>
2018-10-14 17:56:03 +0800

    factory and faker test

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -11,12 +11,7 @@ class ArticlesTableSeeder extends Seeder
      */
     public function run()
     {
-        for($i = 1;$i <= 20; $i++){
-            \App\Models\Article::create([
-                'title' => "測試假文章".$i,
-                'content' => "內容測試",
-                'post_date' => date('Y-m-d H:i:s'),
-            ]);
-        }
+        \App\Models\Article::truncate();
+        factory(\App\Models\Article::class,10)->create();
     }
 }
